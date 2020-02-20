b4d5bc1d0e9e7fbf78fbbc8a50b31ef43f1c0536
Konstantin V. Borisov <smeagol74@gmail.com>
2020-02-21 01:11:06 +0700

    + toc div class styling

--- lib/redmine_global_toc/global_toc.rb
+++ lib/redmine_global_toc/global_toc.rb
@@ -3,22 +3,19 @@ module RedmineGlobalToc::GlobalToc
   Redmine::WikiFormatting::Macros.register do
     desc "Add global-toc macro to Wiki. Example: {{global_toc}}"
     macro :global_toc do |obj, args|
-      out = ''.html_safe
-      out << content_tag(:div) do
-        _textile = ''
-        Project.project_tree(Project.all) do |project, level = 0|
-          _printable = RedmineGlobalToc::GlobalToc.project_printable?(project)
-          _wiki = project.visible? && (project.wiki != nil)
-          if _printable
-            if _wiki
-              _textile = _textile + "#{"*" * (level + 1)} [[#{project.identifier}:|#{project.name}]]\n"
-            else
-              _textile = _textile + "#{"*" * (level + 1)} #{project.name}\n"
-            end
+      _textile = ''
+      Project.project_tree(Project.all) do |project, level = 0|
+        _printable = RedmineGlobalToc::GlobalToc.project_printable?(project)
+        _wiki = project.visible? && (project.wiki != nil)
+        if _printable
+          if _wiki
+            _textile = _textile + "#{"*" * (level + 1)} [[#{project.identifier}:|#{project.name}]]\n"
+          else
+            _textile = _textile + "#{"*" * (level + 1)} #{project.name}\n"
           end
         end
-        concat content_tag(:div, textilizable(_textile, :object => obj, :headings => false))
       end
+      content_tag(:div, textilizable(_textile, :object => obj, :headings => false), :class => 'global-toc')
     end
   end
 
