bbfbadcd5dde2310435307d07a496537ffa877be
Konstantin V. Borisov <smeagol74@gmail.com>
2020-02-20 16:00:23 +0700

    initial

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_global_toc do
+  name 'Redmine Global Toc plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
